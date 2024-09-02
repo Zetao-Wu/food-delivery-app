c6d47ec6c31628b10978e131044fc62a3bbc969c
Zetao <zetao_wu@brown.edu>
2024-09-02 16:47:36 -0400

    adding functionality to navlinks and smooth scroll

--- frontend/src/components/AppDownload.jsx
+++ frontend/src/components/AppDownload.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const AppDownload = () => {
   return (
-    <div className='m-auto mt-[100px] text-2xl text-center font-semibold' id='app-downlaod'>
+    <div className='m-auto mt-[100px] text-2xl text-center font-semibold' id='app-download'>
       <p>For Better Experience, Download <br /> <span className='text-orange-500'>Tomato</span> App</p>
       <div className='flex justify-center gap-2 mt-[40px]'>
         <img className='w-[150px] cursor-pointer hover:scale-105' src={assets.play_store} alt='Google Play Store'/>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [menu, setMenu] = useState("menu");
 
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   return (
     <div className="p-6 flex justify-between items-center">
-      {/* Logo with responsive size */}
       <img src={assets.logo} alt="Logo" className="w-[60px] sm:w-[100px]" />
 
-      {/* Desktop Menu, hidden on smaller screens */}
       <ul className="hidden lg:flex list-none gap-20 text-[#49557e] text-md font-semibold">
-        <li
+        <Link
+          to="/"
           onClick={() => setMenu("home")}
           className={
             menu === "home"
@@ -20,9 +27,12 @@ const Navbar = () => {
           }
         >
           Home
-        </li>
+        </Link>
         <li
-          onClick={() => setMenu("menu")}
+          onClick={() => {
+            setMenu("menu");
+            scrollToSection("explore-menu");
+          }}
           className={
             menu === "menu"
               ? "pb-[2px] border-b-[2px] border-orange-500 cursor-pointer"
@@ -32,7 +42,10 @@ const Navbar = () => {
           Menu
         </li>
         <li
-          onClick={() => setMenu("mobile-app")}
+          onClick={() => {
+            setMenu("mobile-app");
+            scrollToSection("app-download");
+          }}
           className={
             menu === "mobile-app"
               ? "pb-[2px] border-b-[2px] border-orange-500 cursor-pointer"
@@ -42,7 +55,10 @@ const Navbar = () => {
           Mobile-App
         </li>
         <li
-          onClick={() => setMenu("contact-us")}
+          onClick={() => {
+            setMenu("contact-us");
+            scrollToSection("footer");
+          }}
           className={
             menu === "contact-us"
               ? "pb-[2px] border-b-[2px] border-orange-500 cursor-pointer"
@@ -53,15 +69,20 @@ const Navbar = () => {
         </li>
       </ul>
 
-      {/* Icons and Sign In button with responsive size */}
       <div className="flex items-center gap-[20px] lg:gap-[40px]">
-        {/* Adjust icon sizes for smaller screens */}
-        <img src={assets.search_icon} alt="Search Icon" className="w-5 h-5 lg:w-8 lg:h-8" />
+        <img
+          src={assets.search_icon}
+          alt="Search Icon"
+          className="w-5 h-5 lg:w-8 lg:h-8"
+        />
         <div className="relative">
-          <img src={assets.basket_icon} alt="Basket Icon" className="w-5 h-5 lg:w-8 lg:h-8" />
+          <img
+            src={assets.basket_icon}
+            alt="Basket Icon"
+            className="w-5 h-5 lg:w-8 lg:h-8"
+          />
           <div className="absolute min-w-[10px] min-h-[10px] bg-orange-500 rounded-[5px] top-[-4px] right-[-4px]"></div>
         </div>
-        {/* Adjust button size for smaller screens */}
         <button className="bg-transparent text-[10px] lg:text-[16px] border border-red-700 p-1 lg:py-4 lg:px-8 rounded-2xl cursor-pointer hover:bg-[#fff4f2]">
           Sign In
         </button>
