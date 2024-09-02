01ce73ffe4bb797a8800b236b717f12755e5308c
Zetao <zetao_wu@brown.edu>
2024-09-02 10:33:22 -0400

    finish food category sorting functionalitiy

--- frontend/src/components/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay.jsx
@@ -10,16 +10,20 @@ const FoodDisplay = ({ category }) => {
       <h2 className="font-semibold text-3xl">Top Dishes</h2>
       {/* Set up grid to have four columns */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-[50px] gap-7 mt-[30px]">
-        {food_list.map((item, index) => (
-          <FoodItem
-            key={index}
-            id={item._id}
-            name={item.name}
-            description={item.description}
-            price={item.price}
-            image={item.image}
-          />
-        ))}
+        {food_list.map((item, index) => {
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+        })}
       </div>
     </div>
   );
