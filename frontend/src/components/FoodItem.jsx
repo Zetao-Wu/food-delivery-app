f6adeaaa54ba6c41c60b541ec5a3a8473ec00a21
Zetao <zetao_wu@brown.edu>
2024-09-02 10:23:55 -0400

    updating food item component to use StoreContext instead

--- frontend/src/components/FoodItem.jsx
+++ frontend/src/components/FoodItem.jsx
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
-import { assets } from '../assets/assets';
+import { useContext } from "react";
+import { assets } from "../assets/assets";
+import { StoreContext } from "../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0)
-  
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className="w-full m-auto rounded-[15px] shadow-md flex flex-col h-full">
       <div className="relative">
         <img src={image} alt="Food Image" className="w-full rounded-t-[15px]" />
-        {!itemCount ? (
-          <img src={assets.add_icon_white} alt='White Icon' className='w-[35px] absolute bottom-[15px] right-[15px] cursor-pointer rounded-[50%]' onClick={() => setItemCount(prev => prev + 1)}/>
+        {!cartItems[id] ? (
+          <img
+            src={assets.add_icon_white}
+            alt="White Icon"
+            className="w-[35px] absolute bottom-[15px] right-[15px] cursor-pointer rounded-[50%]"
+            onClick={() => addToCart(id)}
+          />
         ) : (
-          <div className='absolute bottom-[15px] right-[15px] flex items-center gap-[10px] p-[6px] rounded-[50px] bg-white'>
-            <img src={assets.remove_icon_red} alt='red icon' onClick={() => setItemCount(prev => prev - 1)} className='w-[30px]'/>
-            <p>{itemCount}</p>
-            <img src={assets.add_icon_green} alt='green icon' onClick={() => setItemCount(prev => prev + 1)} className='w-[30px]'/>
+          <div className="absolute bottom-[15px] right-[15px] flex items-center gap-[10px] p-[6px] rounded-[50px] bg-white">
+            <img
+              src={assets.remove_icon_red}
+              alt="red icon"
+              onClick={() => removeFromCart(id)}
+              className="w-[30px]"
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              src={assets.add_icon_green}
+              alt="green icon"
+              onClick={() => addToCart(id)}
+              className="w-[30px]"
+            />
           </div>
         )}
       </div>
@@ -28,7 +43,9 @@ const FoodItem = ({ id, name, price, description, image }) => {
 
         <p className="text-[#676767] text-[12px] mb-[10px]">{description}</p>
 
-        <p className="text-orange-500 text-[22px] font-semibold my-[10px]">${price}</p>
+        <p className="text-orange-500 text-[22px] font-semibold my-[10px]">
+          ${price}
+        </p>
       </div>
     </div>
   );
