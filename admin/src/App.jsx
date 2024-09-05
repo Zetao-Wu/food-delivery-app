92980b54f498857818ea8e9e9fd24375bd86fd39
Zetao <zetao_wu@brown.edu>
2024-09-05 18:06:32 -0400

    change url as a prop within the app.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-
+  const url = 'http://localhost:4000'
   return (
     <div>
     <ToastContainer />
@@ -19,9 +19,9 @@ const App = () => {
       <div className='flex'>
         <Sidebar />
         <Routes>
-          <Route path="/add" element={<Add />}/>
-          <Route path="/list" element={<List />}/>
-          <Route path="/orders" element={<Orders />}/>
+          <Route path="/add" element={<Add url={url}/>}/>
+          <Route path="/list" element={<List url={url}/>}/>
+          <Route path="/orders" element={<Orders url={url}/>}/>
         </Routes>
       </div>
     </div>
