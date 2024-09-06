5b742eaa83df40826c06b7d5a8b3efa30c96f733
Zetao <zetao_wu@brown.edu>
2024-09-06 07:35:29 -0400

    implemented the api for addToCart functionality and auth middleware using JWT verify

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { removeFromCart, getCart, addToCart } from '../controllers/cartController';
+import { removeFromCart, getCart, addToCart } from '../controllers/cartController.js';
+import authMiddleware from '../middleware/auth.js';
 
 const cartRouter = express.Router();
 
-cartRouter.post("/add", addToCart);
-cartRouter.post("/remove", removeFromCart);
-cartRouter.post("/get", getCart);
+cartRouter.post("/add", authMiddleware, addToCart);
+cartRouter.post("/remove", authMiddleware ,removeFromCart);
+cartRouter.post("/get", authMiddleware, getCart);
 
 
 
