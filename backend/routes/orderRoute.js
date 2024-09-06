013d9a57d53d08ad4cd27ae51cad7288f280bb03
Zetao <zetao_wu@brown.edu>
2024-09-06 19:39:04 -0400

    hook up the verifypage with the backend and stripe

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder } from '../controllers/orderController.js';
-import { verify } from 'jsonwebtoken';
+import { placeOrder,verifyOrder } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
 
