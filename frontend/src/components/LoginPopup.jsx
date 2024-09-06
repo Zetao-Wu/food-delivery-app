20af88b9246b99a687be7264b1d7e200b1a3840d
Zetao <zetao_wu@brown.edu>
2024-09-05 21:08:14 -0400

    complete register and login ui functionalities

--- frontend/src/components/LoginPopup.jsx
+++ frontend/src/components/LoginPopup.jsx
@@ -1,12 +1,46 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
+import { StoreContext } from "../context/StoreContext";
+import axios from 'axios'
 
 const LoginPopup = ({ setShowLogin }) => {
   const [currState, setCurrState] = useState("Login");
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const {url, setToken} = useContext(StoreContext);
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({...data, [name]: value}))
+  }
+
+  const onLogin = async (event) => {
+    event.preventDefault();
+    let newUrl = url;
+    if (currState === "Login") {
+      newUrl += '/api/user/login'
+    } else {
+      newUrl += '/api/user/register'
+    }
+
+    const response = await axios.post(newUrl, data);
+     if (response.data.success) {
+      setToken(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      setShowLogin(false);
+     } else {
+      alert(response.data.message)
+     }
+  } 
+
 
   return (
     <div className="absolute z-10 w-full h-full bg-[#00000090] grid">
-      <form className="place-self-center max-w-[min(23vw,_330px)] text-[#808080] bg-white flex flex-col gap-[25px] py-[25px] px-[30px] rounded-[8px] text-[14px]">
+      <form onSubmit={onLogin} className="place-self-center max-w-[min(23vw,_330px)] text-[#808080] bg-white flex flex-col gap-[25px] py-[25px] px-[30px] rounded-[8px] text-[14px]">
         <div className="flex justify-between items-center text-black">
           <h2 className="text-2xl font-semibold">{currState}</h2>
           <img
@@ -18,12 +52,12 @@ const LoginPopup = ({ setShowLogin }) => {
         </div>
         <div className="flex flex-col gap-[20px]">
           {currState !== "Login" && (
-            <input type="text" placeholder="Username" required className="outline-none border p-[10px] rounded-lg border-black"/>
+            <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder="Username" required className="outline-none border p-[10px] rounded-lg border-black"/>
           )}
-          <input type="email" placeholder="Email" required className="outline-none border p-[10px] rounded-lg border-black"/>
-          <input type="password" placeholder="Password" required className="outline-none border p-[10px] rounded-lg border-black"/>
+          <input name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder="Email" required className="outline-none border p-[10px] rounded-lg border-black"/>
+          <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder="Password" required className="outline-none border p-[10px] rounded-lg border-black"/>
         </div>
-        <button className="border border-black p-[10px] rounded-lg text-white bg-orange-500 text-[15px] hover:opacity-85">{currState === "Sign Up" ? "Create Account" : "Login"}</button>
+        <button type='submit' className="border border-black p-[10px] rounded-lg text-white bg-orange-500 text-[15px] hover:opacity-85">{currState === "Sign Up" ? "Create Account" : "Login"}</button>
         <div className="flex items-start gap-[8px] mt-[-10px]">
           <input className='mt-[5px] cursor-pointer' type="checkbox" required />
           <p>By continuing, I agree to the terms of use & privacy policy.</p>
