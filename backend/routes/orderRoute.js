f474ac49abe3826c70742b2261b61786e482a661
Zetao <zetao_wu@brown.edu>
2024-09-06 19:20:45 -0400

    implement verify order backend api and initalize the verify page

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js';
 import { placeOrder } from '../controllers/orderController.js';
+import { verify } from 'jsonwebtoken';
 
 const orderRouter = express.Router();
 
 orderRouter.post('/place', authMiddleware, placeOrder)
+orderRouter.post('/verify', verifyOrder);
 
 
 
---
+++ frontend/src/pages/Verify.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Verify = () => {
+  return (
+    <div>
+      a
+    </div>
+  )
+}
+
+export default Verify
