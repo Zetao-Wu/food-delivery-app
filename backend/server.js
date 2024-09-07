443aaf631e68896e13c756d263585d18c1b7fa81
Zetao-Wu <141077542+Zetao-Wu@users.noreply.github.com>
2024-09-06 22:52:20 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(Server is running on http://localhost:${PORT});
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
+
