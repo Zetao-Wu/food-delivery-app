96d1dfb2baa0d192cc0c4bd061021038abee4d17
Zetao-Wu <141077542+Zetao-Wu@users.noreply.github.com>
2024-09-06 22:44:14 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';  // Import path module
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
-
 
 
 // app config
@@ -19,19 +19,20 @@ app.use(cors());
 // db connection
 connectDB();
 
+// Serving static files for images (uploads)
+const __dirname = path.resolve();  // Get the directory name
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
+
 // api endpoints
 app.use('/api/food', foodRouter);
-app.use('/images', express.static('uploads'));
 app.use('/api/user', userRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
 
-
 app.get("/", (req, res) => {
-    res.send("API is Working")
+    res.send("API is Working");
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
